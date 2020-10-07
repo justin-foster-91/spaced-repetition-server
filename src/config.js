f845667b423ae468ebcc3232c70ccff670294734
Sonali Najera <sonalitzul@gmail.com>
2020-10-07 10:57:09 -0700

    fixed server side error

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL,
+  DATABASE_URL: process.env.DB_URL
+    || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
